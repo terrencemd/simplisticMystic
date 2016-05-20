7683b04d5b16876ea08131d880e477d875f040e3
Terrence <tdusell07@yahoo.com>
2016-05-20 00:27:59 -0400

    Initial Commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,13 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    
+        'ember-bootstrap': {
+            'importBootstrapTheme': true
+        }
   });
+
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
